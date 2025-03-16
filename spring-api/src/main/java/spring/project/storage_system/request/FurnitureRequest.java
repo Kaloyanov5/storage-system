d14f5062fd1f2268f8564dc4b3b3fba43153b1e9
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-03-16 21:46:51 +0200

    Added `FurnitureRequest` for editing a furniture entity

---
+++ spring-api/src/main/java/spring/project/storage_system/request/FurnitureRequest.java
@@ -0,0 +1,40 @@
+package spring.project.storage_system.request;
+
+public class FurnitureRequest {
+    private String name;
+    private String category;
+    private double price;
+    private String imageName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
